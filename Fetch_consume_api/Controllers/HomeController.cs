52049dd1f04dd8db531e28e00c7a1177b8b01ffb
Administrator <Administrator@F6A0A8A8292C59E>
2023-07-26 14:32:13 +0530

    Add project files.

---
+++ Fetch_consume_api/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Fetch_consume_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Fetch_consume_api.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IActionResult PostData()
+        {
+            return View();
+        }
+        public IActionResult User()
+        {
+            return User();
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Fetch_consume_api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fetch_consume_api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
